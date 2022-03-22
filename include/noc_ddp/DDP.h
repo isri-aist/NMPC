7e8e26558b62ac4a9cbe40279c84d3cb70586a74
Masaki Murooka <m-murooka@aist.go.jp>
2022-03-22 13:38:40 +0900

    implement DDP. not tested yet.

--- include/noc_ddp/DDP.h
+++ include/noc_ddp/DDP.h
@@ -2,40 +2,78 @@
 
 #pragma once
 
+#include <memory>
 #include <vector>
 
 #include <Eigen/Dense>
 
 namespace NOC
 {
-/** \brief Class for non-linear optimal control problem. */
-class NOCProblem
+/** \brief DDP problem.
+    \tparam StateDim state dimension
+    \tparam InputDim input dimension
+ */
+template<int StateDim, int InputDim>
+class DDPProblem
 {
 public:
-  /** \brief Type of state vector. */
+  /** \brief Type of vector of state dimension. */
   using StateDimVector = Eigen::Matrix<double, StateDim, 1>;
 
-  /** \brief Type of input vector. */
+  /** \brief Type of vector of input dimension. */
   using InputDimVector = Eigen::Matrix<double, InputDim, 1>;
 
+  /** \brief Type of matrix of state x state dimension. */
   using StateStateDimMatrix = Eigen::Matrix<double, StateDim, StateDim>;
 
+  /** \brief Type of matrix of input x input dimension. */
   using InputInputDimMatrix = Eigen::Matrix<double, InputDim, InputDim>;
 
+  /** \brief Type of matrix of state x input dimension. */
   using StateInputDimMatrix = Eigen::Matrix<double, StateDim, InputDim>;
 
+  /** \brief Type of matrix of input x state dimension. */
   using InputStateDimMatrix = Eigen::Matrix<double, InputDim, StateDim>;
 
 public:
-  NOCProblem() {}
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+  /** \brief Constructor. */
+  DDPProblem(int state_dim = StateDim, int input_dim = InputDim);
+
+  /** \brief Gets the state dimension. */
+  inline int stateDim() const
+  {
+    return state_dim_;
+  }
+
+  /** \brief Gets the input dimension. */
+  inline int inputDim() const
+  {
+    return input_dim_;
+  }
+
+  /** \brief Calculate discrete state equation.
+      \param x current state (x[k])
+      \param u current input (u[k])
+      \returns next state (x[k+1])
+   */
   virtual StateDimVector stateEq(const StateDimVector & x, const InputDimVector & u) const = 0;
 
+  /** \brief Calculate running cost.
+      \param x current state (x[k])
+      \param u current input (u[k])
+      \returns running cost (L[k])
+   */
   virtual double runningCost(const StateDimVector & x, const InputDimVector & u) const = 0;
 
+  /** \brief Calculate terminal cost.
+      \param x current state (x[k])
+      \returns terminal cost (phi[k])
+   */
   virtual double terminalCost(const StateDimVector & x) const = 0;
 
-  /** \brief Calculate first-order derivatives of state equation.
+  /** \brief Calculate first-order derivatives of discrete state equation.
       \param x state
       \param u input
       \param state_eq_deriv_x first-order derivative of state equation w.r.t. state
@@ -46,7 +84,7 @@ public:
                                Eigen::Ref<StateStateDimMatrix> state_eq_deriv_x,
                                Eigen::Ref<StateInputDimMatrix> state_eq_deriv_u) const = 0;
 
-  /** \brief Calculate first-order and second-order derivatives of state equation.
+  /** \brief Calculate first-order and second-order derivatives of discrete state equation.
       \param x state
       \param u input
       \param state_eq_deriv_x first-order derivative of state equation w.r.t. state
@@ -94,7 +132,7 @@ public:
   /** \brief Calculate first-order derivatives of terminal cost.
       \param x state
       \param terminal_cost_deriv_x first-order derivative of terminal cost w.r.t. state
-   */
+  */
   virtual void calcTerminalCostDeriv(const StateDimVector & x,
                                      Eigen::Ref<StateDimVector> terminal_cost_deriv_x) const = 0;
 
@@ -102,18 +140,51 @@ public:
       \param x state
       \param terminal_cost_deriv_x first-order derivative of terminal cost w.r.t. state
       \param terminal_cost_deriv_xx second-order derivative of terminal cost w.r.t. state
-   */
+  */
   virtual void calcTerminalCostDeriv(const StateDimVector & x,
                                      Eigen::Ref<StateDimVector> terminal_cost_deriv_x,
                                      Eigen::Ref<StateStateDimMatrix> terminal_cost_deriv_xx) const = 0;
+
+protected:
+  //! State dimension
+  const int state_dim_ = 0;
+
+  //! Input dimension
+  const int input_dim_ = 0;
 };
 
-/** \brief Class for algorithm of differential dynamic programming.
+/** \brief DDP solver.
+    \tparam StateDim state dimension
+    \tparam InputDim input dimension
 
-    Ref https://www.mathworks.com/matlabcentral/fileexchange/52069-ilqg-ddp-trajectory-optimization
+    See the following for a detailed algorithm.
+      - Y Tassa, T Erez, E Todorov. Synthesis and stabilization of complex behaviors through online trajectory
+   optimization. IROS2012.
+      - Y Tassa, N Mansard, E Todorov. Control-limited differential dynamic programming. ICRA2014.
+      - https://www.mathworks.com/matlabcentral/fileexchange/52069-ilqg-ddp-trajectory-optimization
  */
-class DDP
+template<int StateDim, int InputDim>
+class DDPSolver
 {
+public:
+  /** \brief Type of vector of state dimension. */
+  using StateDimVector = Eigen::Matrix<double, StateDim, 1>;
+
+  /** \brief Type of vector of input dimension. */
+  using InputDimVector = Eigen::Matrix<double, InputDim, 1>;
+
+  /** \brief Type of matrix of state x state dimension. */
+  using StateStateDimMatrix = Eigen::Matrix<double, StateDim, StateDim>;
+
+  /** \brief Type of matrix of input x input dimension. */
+  using InputInputDimMatrix = Eigen::Matrix<double, InputDim, InputDim>;
+
+  /** \brief Type of matrix of state x input dimension. */
+  using StateInputDimMatrix = Eigen::Matrix<double, StateDim, InputDim>;
+
+  /** \brief Type of matrix of input x state dimension. */
+  using InputStateDimMatrix = Eigen::Matrix<double, InputDim, StateDim>;
+
 public:
   /*! \brief Configuration. */
   struct Configuration
@@ -124,8 +195,20 @@ public:
     //! Whether to use second-order derivatives of state equation
     bool use_state_eq_second_derivative = false;
 
+    //! Maximum iteration of optimization loop
+    int max_iter = 500;
+
+    //! Number of steps in horizon
+    int horizon_steps = 100;
+
     //! Regularization type (1: Quu + lambda * I, 2: Vxx + lambda * I)
     int reg_type = 1;
+
+    //! Initial regularization coefficient
+    double initial_lambda = 1e-6;
+
+    //! Initial scaling factor of regularization coefficient
+    double initial_dlambda = 1.0;
 
     //! Increasing/decreasing factor of regularization coefficient scaling
     double lambda_factor = 1.6;
@@ -141,40 +224,54 @@ public:
 
     //! Termination threshold of regularization coefficient
     double lambda_thre = 1e-5;
+
+    //! List of alpha (scaling factor of k)
+    Eigen::VectorXd alpha_list = Eigen::VectorXd::LinSpaced(11, 0, -3);
+
+    //! Allowable threshold of cost update ratio
+    double cost_update_ratio_thre = 0;
+
+    //! Termination threshold of cost update
+    double cost_update_thre = 1e-7;
   };
 
-  /*! \brief Derivatives of non-linear optimal control problem. */
-  class NOCProblemDerivative
+  /*! \brief Derivatives of DDP problem. */
+  struct Derivative
   {
-  public:
-    NOCProblemDerivative() {}
+    /** \brief Constructor. */
+    Derivative() {}
 
-    NOCProblemDerivative(size_t state_dim)
+    /** \brief Constructor.
+        \param state_dim state dimension
+     */
+    Derivative(int state_dim)
     {
       setStateDim(state_dim);
     }
 
-    void setStateDim(size_t state_dim)
+    /** \brief Set state dimension.
+        \param state_dim state dimension
+     */
+    void setStateDim(int state_dim)
     {
       Fxx.resize(state_dim);
       Fuu.resize(state_dim);
       Fxu.resize(state_dim);
     }
 
-  public:
     //! First-order derivative of state equation w.r.t. state
     StateStateDimMatrix Fx;
 
     //! First-order derivative of state equation w.r.t. input
     StateInputDimMatrix Fu;
 
-    //! Second-order derivative of state equation w.r.t. state
+    //! Second-order derivative of state equation w.r.t. state (tensor of rank 3)
     std::vector<StateStateDimMatrix> Fxx;
 
-    //! Second-order derivative of state equation w.r.t. input
+    //! Second-order derivative of state equation w.r.t. input (tensor of rank 3)
     std::vector<InputInputDimMatrix> Fuu;
 
-    //! Second-order derivative of state equation w.r.t. state and input
+    //! Second-order derivative of state equation w.r.t. state and input (tensor of rank 3)
     std::vector<StateInputDimMatrix> Fxu;
 
     //! First-order derivative of running cost w.r.t. state
@@ -193,36 +290,111 @@ public:
     StateInputDimMatrix Lxu;
   };
 
-public:
-  /** \brief Constructor.
-      \param rb robot
-  */
-  DDP();
+  /*! \brief Data to trace optimization loop. */
+  struct TraceData
+  {
+    //! Iteration of optimization loop
+    int iter = 0;
 
-protected:
+    //! Total cost
+    double cost = 0;
+
+    //! Regularization coefficient
+    double lambda = 0;
+
+    //! Scaling factor of regularization coefficient
+    double dlambda = 0;
+
+    //! Scaling factor of k
+    double alpha = 0;
+
+    //! Norm of relative values of k and u
+    double k_rel_norm = 0;
+
+    //! Actual update value of cost
+    double cost_update_actual = 0;
+
+    //! Expected update value of cost
+    double cost_update_expected = 0;
+
+    //! Ratio of actual and expected update values of cost
+    double cost_update_ratio = 0;
+
+    //! Duration to calculate derivatives
+    double duration_derivative = 0;
+
+    //! Duration to process backward pass
+    double duration_backward = 0;
+
+    //! Duration to process forward pass
+    double duration_forward = 0;
+  };
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
   /** \brief Constructor.
+      \param problem DDP problem
+  */
+  DDPSolver(const std::shared_ptr<DDPProblem<StateDim, InputDim>> & problem);
+
+  /** \brief Solve optimization.
+      \param current_x current state
+      \param initial_u_list initial sequence of input
       \return whether the process is finished successfully
   */
-  bool procBackwardPass();
+  bool solve(const StateDimVector & current_x, const std::vector<InputDimVector> & initial_u_list);
+
+protected:
+  /** \brief Process one iteration.
+      \param iter current iteration
+      \return 0 for continue, 1 for terminate, -1 for failure
+  */
+  int procOnce(int iter);
+
+  /** \brief Process backward pass.
+      \return whether the process is finished successfully
+  */
+  bool backwardPass();
+
+  /** \brief Process forward pass.
+      \param alpha scaling factor of k
+  */
+  void forwardPass(double alpha);
 
 public:
   //! Configuration
   Configuration config_;
 
   //! Non-linear optimal control problem
-  std::shared_ptr<NOCProblem> problem_;
+  std::shared_ptr<DDPProblem<StateDim, InputDim>> problem_;
+
+  //! Sequence of trace data
+  std::vector<TraceData> trace_data_list_;
 
   //! Regularization coefficient
-  double lambda_ = 1e-6;
+  double lambda_ = 0;
 
   //! Scaling factor of regularization coefficient
-  double dlambda_ = 1.0;
+  double dlambda_ = 0;
 
   //! Sequence of state (x[0], ..., x[N-1], x[N])
   std::vector<StateDimVector> x_list_;
 
   //! Sequence of input (u[0], ..., u[N-1])
   std::vector<InputDimVector> u_list_;
+
+  //! Sequence of cost (L[0], ..., L[N-1], phi[N])
+  Eigen::VectorXd cost_list_;
+
+  //! Sequence of candidate state
+  std::vector<StateDimVector> x_candidate_list_;
+
+  //! Sequence of candidate input
+  std::vector<InputDimVector> u_candidate_list_;
+
+  //! Sequence of candidate cost
+  Eigen::VectorXd cost_candidate_list_;
 
   //! Sequence of feedforward term for input (k[0], ..., k[N-1])
   std::vector<InputDimVector> k_list_;
@@ -231,12 +403,17 @@ public:
   std::vector<InputStateDimMatrix> K_list_;
 
   //! Sequence of derivatives
-  std::vector<NOCProblemDerivative> derivative_list_;
+  std::vector<Derivative> derivative_list_;
 
   //! First-order derivative of value in last step of horizon
   StateDimVector last_Vx_;
 
   //! Second-order derivative of value in last step of horizon
   StateStateDimMatrix last_Vxx_;
+
+  //! Expected update of value
+  Eigen::Vector2d dV_;
 };
 } // namespace NOC
+
+#include <noc_ddp/DDP.hpp>
---
+++ include/noc_ddp/DDP.hpp
@@ -0,0 +1,413 @@
+/* Author: Masaki Murooka */
+
+#include <chrono>
+#include <iostream>
+
+namespace NOC
+{
+template<int StateDim, int InputDim>
+DDPProblem<StateDim, InputDim>::DDPProblem(int state_dim, int input_dim) : state_dim_(state_dim), input_dim_(input_dim)
+{
+  // Check dimension is positive
+  if(state_dim_ <= 0)
+  {
+    throw std::runtime_error("state_dim must be positive: " + std::to_string(state_dim_) + " <= 0");
+  }
+  if(input_dim_ < 0)
+  {
+    throw std::runtime_error("input_dim must be non-negative: " + std::to_string(input_dim_) + " < 0");
+  }
+
+  // Check dimension consistency
+  if constexpr(StateDim != Eigen::Dynamic)
+  {
+    if(state_dim_ != StateDim)
+    {
+      throw std::runtime_error("state_dim is inconsistent with template parameter: " + std::to_string(state_dim_)
+                               + " != " + std::to_string(StateDim));
+    }
+  }
+  if constexpr(InputDim != Eigen::Dynamic)
+  {
+    if(input_dim_ != InputDim)
+    {
+      throw std::runtime_error("input_dim is inconsistent with template parameter: " + std::to_string(input_dim_)
+                               + " != " + std::to_string(InputDim));
+    }
+  }
+}
+
+template<int StateDim, int InputDim>
+DDPSolver<StateDim, InputDim>::DDPSolver(const std::shared_ptr<DDPProblem<StateDim, InputDim>> & problem)
+: problem_(problem)
+{
+}
+
+template<int StateDim, int InputDim>
+bool DDPSolver<StateDim, InputDim>::solve(const StateDimVector & current_x,
+                                          const std::vector<InputDimVector> & initial_u_list)
+{
+  // Initialize variables
+  trace_data_list_.clear();
+  lambda_ = config_.initial_lambda;
+  dlambda_ = config_.initial_dlambda;
+
+  // Resize list
+  x_candidate_list_.resize(config_.horizon_steps + 1);
+  u_candidate_list_.resize(config_.horizon_steps);
+  cost_candidate_list_.resize(config_.horizon_steps + 1);
+  derivative_list_.resize(config_.horizon_steps);
+  k_list_.resize(config_.horizon_steps);
+  K_list_.resize(config_.horizon_steps);
+
+  // Initialize state and cost sequence
+  u_list_ = initial_u_list;
+  x_list_.resize(config_.horizon_steps + 1);
+  cost_list_.resize(config_.horizon_steps + 1);
+  x_list_[0] = current_x;
+  for(int i = 0; i < config_.horizon_steps; i++)
+  {
+    x_list_[i + 1] = problem_->stateEq(x_list_[i], u_list_[i]);
+    cost_list_[i] = problem_->runningCost(x_list_[i], u_list_[i]);
+  }
+  cost_list_[config_.horizon_steps] = problem_->terminalCost(x_list_[config_.horizon_steps]);
+
+  // Optimization loop
+  int retval = 0;
+  for(int iter = 0; iter < config_.max_iter; iter++)
+  {
+    retval = procOnce(iter);
+    if(retval != 0)
+    {
+      break;
+    }
+  }
+
+  return retval == 1;
+}
+
+template<int StateDim, int InputDim>
+int DDPSolver<StateDim, InputDim>::procOnce(int iter)
+{
+  // Append trace data
+  trace_data_list_.push_back(TraceData());
+  auto & trace_data = trace_data_list_.back();
+
+  trace_data.iter = iter;
+
+  // Step 1: differentiate dynamics and cost along new trajectory
+  {
+    auto start_time = std::chrono::system_clock::now();
+
+    for(int i = 0; i < config_.horizon_steps; i++)
+    {
+      auto & derivative = derivative_list_[i];
+      derivative.setStateDim(config_.use_state_eq_second_derivative ? problem_->stateDim() : 0);
+
+      const StateDimVector & x = x_list_[i];
+      const InputDimVector & u = u_list_[i];
+      if(config_.use_state_eq_second_derivative)
+      {
+        problem_->calcStateqDeriv(x, u, derivative.Fx, derivative.Fu, derivative.Fxx, derivative.Fuu, derivative.Fxu);
+      }
+      else
+      {
+        problem_->calcStateqDeriv(x, u, derivative.Fx, derivative.Fu);
+      }
+      problem_->calcRunningCostDeriv(x, u, derivative.Lx, derivative.Lu, derivative.Lxx, derivative.Luu,
+                                     derivative.Lxu);
+    }
+    problem_->calcTerminalCostDeriv(x_list_[config_.horizon_steps], last_Vx_, last_Vxx_);
+
+    trace_data.duration_derivative =
+        1e3
+        * std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now() - start_time)
+              .count();
+  }
+
+  // STEP 2: backward pass, compute optimal control law and cost-to-go
+  {
+    auto start_time = std::chrono::system_clock::now();
+
+    while(!backwardPass())
+    {
+      // Increase lambda
+      dlambda_ = std::max(dlambda_ * config_.lambda_factor, config_.lambda_factor);
+      lambda_ = std::max(lambda_ * dlambda_, config_.lambda_min);
+      if(lambda_ > config_.lambda_max)
+      {
+        if(config_.verbose_print)
+        {
+          std::cout << "[DDP] Failure due to large lambda." << std::endl;
+        }
+        return -1; // Failure
+      }
+    }
+
+    trace_data.duration_backward =
+        1e3
+        * std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now() - start_time)
+              .count();
+  }
+
+  // Check for termination due to small gradient
+  double k_rel_norm = 0;
+  for(int i = 0; i < config_.horizon_steps; i++)
+  {
+    k_rel_norm = std::max(k_rel_norm, k_list_[i].norm() / (u_list_[i].norm() + 1.0));
+  }
+  trace_data.k_rel_norm = k_rel_norm;
+  if(k_rel_norm < config_.k_rel_norm_thre && lambda_ < config_.lambda_thre)
+  {
+    if(config_.verbose_print)
+    {
+      std::cout << "[DDP] Terminate due to small gradient." << std::endl;
+    }
+    return 1; // Terminate
+  }
+
+  // STEP 3: forward pass, line-search to find new control sequence, trajectory, cost
+  // \todo Parallel line-search by broadcasting
+  bool forward_pass_success = false;
+  double cost_update_actual = 0;
+  {
+    auto start_time = std::chrono::system_clock::now();
+
+    double cost_update_expected = 0;
+    double cost_update_ratio = 0;
+    for(int i = 0; i < config_.alpha_list.size(); i++)
+    {
+      double alpha = config_.alpha_list[i];
+
+      forwardPass(alpha);
+
+      cost_update_actual = cost_list_.sum() - cost_candidate_list_.sum();
+      cost_update_expected = -1 * alpha * (dV_[0] + alpha * dV_[1]);
+      cost_update_ratio = cost_update_actual / cost_update_expected;
+      if(cost_update_expected < 0)
+      {
+        std::cout << "[DDP] Value is not expected to decrease." << std::endl;
+        cost_update_ratio = (cost_update_actual >= 0 ? 1 : -1);
+      }
+      if(cost_update_ratio > config_.cost_update_ratio_thre)
+      {
+        forward_pass_success = true;
+        trace_data.alpha = alpha;
+        break;
+      }
+
+      if(i == config_.alpha_list.size() - 1)
+      {
+        trace_data.alpha = 0;
+      }
+    }
+    trace_data.cost_update_actual = cost_update_actual;
+    trace_data.cost_update_expected = cost_update_expected;
+    trace_data.cost_update_ratio = cost_update_ratio;
+
+    trace_data.duration_forward =
+        1e3
+        * std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now() - start_time)
+              .count();
+  }
+
+  // STEP 4: accept step (or not)
+  int retval = 0; // Continue
+  if(forward_pass_success)
+  {
+    // Decrease lambda
+    dlambda_ = std::min(dlambda_ / config_.lambda_factor, 1 / config_.lambda_factor);
+    if(lambda_ > config_.lambda_min)
+    {
+      lambda_ *= dlambda_;
+    }
+    else
+    {
+      lambda_ = 0;
+    }
+
+    // Accept changes
+    x_list_ = x_candidate_list_;
+    u_list_ = u_candidate_list_;
+    cost_list_ = cost_candidate_list_;
+
+    // Check for termination due to small cost update
+    if(cost_update_actual < config_.cost_update_thre)
+    {
+      if(config_.verbose_print)
+      {
+        std::cout << "[DDP] Terminate due to small cost update." << std::endl;
+      }
+      retval = 1; // Terminate
+    }
+  }
+  else
+  {
+    // Increase lambda
+    dlambda_ = std::max(dlambda_ * config_.lambda_factor, config_.lambda_factor);
+    lambda_ = std::max(lambda_ * dlambda_, config_.lambda_min);
+    if(lambda_ > config_.lambda_max)
+    {
+      if(config_.verbose_print)
+      {
+        std::cout << "[DDP] Failure due to large lambda." << std::endl;
+      }
+      retval = -1; // Failure
+    }
+  }
+
+  trace_data.cost = cost_list_.sum();
+  trace_data.lambda = lambda_;
+  trace_data.dlambda = dlambda_;
+
+  return retval;
+}
+
+template<int StateDim, int InputDim>
+bool DDPSolver<StateDim, InputDim>::backwardPass()
+{
+  // To avoid memory allocation costs, the vector and matrix variables are created outside of loop
+  StateDimVector Vx = last_Vx_;
+  StateStateDimMatrix Vxx = last_Vxx_;
+  StateStateDimMatrix Vxx_reg;
+
+  InputDimVector Qu;
+  StateDimVector Qx;
+  InputStateDimMatrix Qux;
+  InputInputDimMatrix Quu;
+  StateStateDimMatrix Qxx;
+  InputStateDimMatrix Qux_reg;
+  InputInputDimMatrix Quu_F;
+
+  InputStateDimMatrix VxFxu;
+  InputInputDimMatrix VxFuu;
+
+  InputDimVector k;
+  InputStateDimMatrix K;
+
+  dV_.setZero();
+
+  for(int i = config_.horizon_steps - 1; i >= 0; i--)
+  {
+    // Get derivatives
+    const StateStateDimMatrix & Fx = derivative_list_[i].Fx;
+    const StateInputDimMatrix & Fu = derivative_list_[i].Fu;
+    const std::vector<StateStateDimMatrix> & Fxx = derivative_list_[i].Fxx;
+    const std::vector<InputInputDimMatrix> & Fuu = derivative_list_[i].Fuu;
+    const std::vector<StateInputDimMatrix> & Fxu = derivative_list_[i].Fxu;
+    const StateDimVector & Lx = derivative_list_[i].Lx;
+    const InputDimVector & Lu = derivative_list_[i].Lu;
+    const StateStateDimMatrix & Lxx = derivative_list_[i].Lxx;
+    const InputInputDimMatrix & Luu = derivative_list_[i].Luu;
+    const StateInputDimMatrix & Lxu = derivative_list_[i].Lxu;
+
+    // Calculate Q
+    Qu = Lu + Fu.transpose() * Vx;
+
+    Qx = Lx + Fx.transpose() * Vx;
+
+    Qux = Lxu.transpose() + Fu.transpose() * Vxx * Fx;
+    if(config_.use_state_eq_second_derivative)
+    {
+      throw std::runtime_error("Vector-tensor product is not implemented yet.");
+      // \todo Need operation to compute a matrix by vector and tensor product
+      // VxFxu = Vx * Fxu;
+      // Qux += VxFxu
+    }
+
+    Quu = Luu + Fu.transpose() * Vxx * Fu;
+    if(config_.use_state_eq_second_derivative)
+    {
+      throw std::runtime_error("Vector-tensor product is not implemented yet.");
+      // \todo Need operation to compute a matrix by vector and tensor product
+      // VxFuu = Vx * Fuu;
+      // Quu += VxFuu;
+    }
+
+    Qxx = Lxx + Fx.transpose() * Vxx * Fx;
+    if(config_.use_state_eq_second_derivative)
+    {
+      throw std::runtime_error("Vector-tensor product is not implemented yet.");
+      // \todo Need operation to compute a matrix by vector and tensor product
+      // Qxx += Vx * Fxx;
+    }
+
+    // Calculate regularization
+    Vxx_reg = Vxx;
+    if(config_.reg_type == 2)
+    {
+      Vxx_reg += lambda_ * StateStateDimMatrix::Identity();
+    }
+
+    Qux_reg = Lxu.transpose() + Fu.transpose() * Vxx_reg * Fx;
+    if(config_.use_state_eq_second_derivative)
+    {
+      Qux_reg += VxFxu;
+    }
+
+    Quu_F = Luu + Fu.transpose() * Vxx_reg * Fu;
+    if(config_.use_state_eq_second_derivative)
+    {
+      Quu_F += VxFuu;
+    }
+    if(config_.reg_type == 1)
+    {
+      Quu_F += lambda_ * InputInputDimMatrix::Identity();
+    }
+
+    // Calculate gains
+    if(config_.with_input_constraint)
+    {
+      // \todo Calculate gains with considering constraints
+      throw std::runtime_error("Input constraint is not supported yet.");
+    }
+    else
+    {
+      Eigen::LLT<InputInputDimMatrix> llt_Quu_F(Quu_F);
+      if(llt_Quu_F.info() == Eigen::NumericalIssue)
+      {
+        if(config_.verbose_print)
+        {
+          std::cout << "[DDP] Quu_F is not positive definite in Cholesky decomposition (LLT)." << std::endl;
+        }
+        return false;
+      }
+      k = -1 * llt_Quu_F.solve(Qu);
+      K = -1 * llt_Quu_F.solve(Qux_reg);
+    }
+
+    // Update cost-to-go approximation
+    dV_ += Eigen::Vector2d(k.transpose() * Qu, 0.5 * k.transpose() * Quu * k);
+    Vx = Qx + K.transpose() * Quu * k + K.transpose() * Qu + Qux.transpose() * k;
+    Vxx = Qxx + K.transpose() * Quu * K + K.transpose() * Qux + Qux.transpose() * K;
+    Vxx = 0.5 * (Vxx + Vxx.transpose());
+
+    // Save gains
+    k_list_[i] = k;
+    K_list_[i] = K;
+  }
+
+  return true;
+}
+
+template<int StateDim, int InputDim>
+void DDPSolver<StateDim, InputDim>::forwardPass(double alpha)
+{
+  // Set initial state
+  x_candidate_list_[0] = x_list_[0];
+
+  for(int i = 0; i < config_.horizon_steps; i++)
+  {
+    // Calculate new input
+    u_candidate_list_[i] = u_list_[i] + alpha * k_list_[i] + K_list_[i] * (x_candidate_list_[i] - x_list_[i]);
+
+    // \todo Impose constraints on input
+
+    // Calculate new state of next step
+    x_candidate_list_[i + 1] = problem_->stateEq(x_candidate_list_[i], u_candidate_list_[i]);
+
+    cost_candidate_list_[i] = problem_->runningCost(x_candidate_list_[i], u_candidate_list_[i]);
+  }
+  cost_candidate_list_[config_.horizon_steps] = problem_->terminalCost(x_candidate_list_[config_.horizon_steps]);
+}
+} // namespace NOC
