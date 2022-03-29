2a44b9dd01e50cb0c22c1be639218ff3fc823c0c
Masaki Murooka <m-murooka@aist.go.jp>
2022-03-29 16:46:24 +0900

    make library header-only.

--- nmpc_ddp/src/DDP.cpp
+++
@@ -1,8 +0,0 @@
-/* Author: Masaki Murooka */
-
-#include <nmpc_ddp/DDP.h>
-
-using namespace nmpc_ddp;
-
-// See https://isocpp.org/wiki/faq/templates#separate-template-class-defn-from-decl
-template class DDPProblem<2, 1>;
